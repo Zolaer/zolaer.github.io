145fa43aa3ee5285583ddf39c3f2f502d3397443
Zolaer <2873832196@qq.com>
2025-02-11 21:28:10 +0800

    Site updated: 2025-02-11 21:28:07

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1739278734081";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1739280486115";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
